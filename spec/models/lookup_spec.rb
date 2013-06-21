ed4eebf2643d959134d876d66984d97c5ee96329
Nikky Southerland <nikky@uw.edu>
2013-06-21 10:34:58 -0700

    Add some specs

--- spec/models/lookup_spec.rb
+++ spec/models/lookup_spec.rb
@@ -4,13 +4,29 @@ require 'spec_helper'
 # check response
 
 describe Lookup do
+  before do
+    @request_object = Lookup.new("nikky")
+  end
   context "#validate_netid" do
-    it "returns true on valid netID"
-    it "returns false on invalid netid"
+    it "returns true on valid netID" do
+      @request_object.netid_interface.should_receive(:validate_netid?).and_return(true)
+      @request_object.validate_netid.should be_true
+    end
+    it "returns false on invalid netid" do
+      @request_object.netid_interface.should_receive(:validate_netid?).and_return(false)
+      @request_object.validate_netid.should be_false
+    end
   end
 
   context "#check_for_mysql" do
-    it "checks for mysql"
+    it "returns hosts array" do
+      @request_object.netid_interface.stub(:check_for_mysql_presence).and_return(true)
+      @request_object.check_for_mysql.size.should eq 4
+    end
+    it "returns false if no MySQL" do
+      @request_object.netid_interface.stub(:check_for_mysql_presence).and_return(false)
+      @request_object.check_for_mysql.should be_false
+    end
   end
 
   context "#get_processes" do
@@ -18,11 +34,21 @@ describe Lookup do
   end
 
   context "#check_for_localhome" do
-    it "checks for localhome"
+    it "returns a host if localhome" do
+      @request_object.netid_interface.stub(:check_for_localhome).and_return("ovid02")
+      @request_object.check_for_localhome.should eq "ovid02"
+    end
+    it "returns false if no localhome" do
+      @request_object.netid_interface.stub(:check_for_localhome).and_return(false)
+      @request_object.check_for_localhome.should be_false
+    end
   end
 
   context "#check_webtype" do
-    it "checks for webtype"
+    it "returns an array" do
+      @request_object.netid_interface.stub(:check_webtype).and_return(%w(depts staff))
+      @request_object.check_webtype.should eq %w(depts staff)
+    end
   end
 
   context "#check_quota" do
